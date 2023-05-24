e50fa01f74d55672538eda073c71f4ba194b5943
ibrahimmohamed6767 <ibrahimmohamed5490@gmail.com>
2023-05-24 12:31:43 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @ac: Num of argu.
+ * @argv: array of arguments
+ * Return: returned 0, or -1 if fail
+ */
+int main(int ac, char **argv)
+{
+char *line = NULL;
+size_t n = 0;
+char **args;
+
+(void)ac;
+
+while (1)
+{
+printf("$ ");
+if (getline(&line, &n, stdin) == -1)
+{
+perror("getline error");
+return (-1);
+}
+args = read_input(line, &n);
+if (args == NULL)
+{
+fprintf(stderr, "Failed to read input\n");
+continue;
+}
+if (strcmp(args[0], "env") == 0)
+{
+execute_env();
+}
+else
+{
+execute(args, argv);
+}
+
+free (args);
+}
+return (0);
+}
